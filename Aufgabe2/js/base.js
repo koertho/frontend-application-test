275ec95229ca1e3b3c7515b524b4c5ae79769335
Thomas <koertho@outlook.de>
2017-04-26 17:22:05 +0200

    Aufgabe 2: JS Slideshow

--- Aufgabe2/js/base.js
+++ Aufgabe2/js/base.js
@@ -1,1 +1,11 @@
-// Logik bitte hier
+
+var slides = document.querySelectorAll('section.content .part');
+var current = 0;
+nextSlide();
+var timer = setInterval(nextSlide, 4000);
+
+function nextSlide() {
+		slides[current].className="part";
+		current = (current+1)%slides.length;
+		slides[current].className = "part active";
+	}
